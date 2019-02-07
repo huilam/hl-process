71c7dc9402d3f9130f2823ac9416ef57e999c77a
onghuilam <onghuilam@gmail.com>
2019-02-07 14:00:06 +0800

    externalise ascii arts

--- src/hl/common/shell/plugins/output/StateOutput.java
+++ src/hl/common/shell/plugins/output/StateOutput.java
@@ -1,6 +1,7 @@
 package hl.common.shell.plugins.output;
 
 import hl.common.shell.HLProcess.ProcessState;
+import hl.common.shell.utils.FileUtil;
 
 public class StateOutput 
 {
@@ -9,60 +10,8 @@ public class StateOutput
 	
 	static
 	{
-		StringBuffer sb = new StringBuffer();
-		sb.append("\n").append("                            OOOOOOOOOO");
-		sb.append("\n").append("                        OOOOOOOOOOOOOOOOOO");
-		sb.append("\n").append("                     OOOOOO  OOOOOOOO  OOOOOO");
-		sb.append("\n").append("                   OOOOOO      OOOO      OOOOOO");
-		sb.append("\n").append("                 OOOOOOOO      OOOO      OOOOOOOO");
-		sb.append("\n").append("                OOOOOOOOOO    OOOOOO    OOOOOOOOOO");
-		sb.append("\n").append("               OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
-		sb.append("\n").append("               OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
-		sb.append("\n").append("               OOOO  OOOOOOOOOOOOOOOOOOOOOOOO  OOOO");
-		sb.append("\n").append("                OOOO  OOOOOOOOOOOOOOOOOOOOOO  OOOO");
-		sb.append("\n").append("                 OOOO   OOOOOOOOOOOOOOOOOOO  OOOO");
-		sb.append("\n").append("                   OOOOO   OOOOOOOOOOOOOO   OOOO");
-		sb.append("\n").append("                     OOOOOO   OOOOOOOO   OOOOOO");
-		sb.append("\n").append("                        OOOOOO        OOOOOO");
-		sb.append("\n").append("                            OOOOOOOOOOO");
-		sb.append("\n");
-		sb.append("\n").append("   ######  ########  ######   #######  ######## ######## #######");
-		sb.append("\n").append("  ##    ##    ##    ##    ##  ##    ##    ##    ##       ##     #");
-		sb.append("\n").append("  ##          ##    ##    ##  ##    ##    ##    ##       ##      #");
-		sb.append("\n").append("   ######     ##    ########  #######     ##    ######   ##      #");
-		sb.append("\n").append("        ##    ##    ##    ##  ##   ##     ##    ##       ##      #");
-		sb.append("\n").append("  ##    ##    ##    ##    ##  ##    ##    ##    ##       ##     # ");
-		sb.append("\n").append("   ######     ##    ##    ##  ##    ##    ##    ######## #######");
-		_SMILEY = sb.toString();
-		
-		sb.setLength(0);
-		sb.append("\n").append("                   #######");
-		sb.append("\n").append("               ###############");
-		sb.append("\n").append("             #####################");
-		sb.append("\n").append("            #######################");
-		sb.append("\n").append("           #########################");
-		sb.append("\n").append("          ###########################");
-		sb.append("\n").append("          ###########################");
-		sb.append("\n").append("          ########   #####   ########");
-		sb.append("\n").append("          ######      ###       #####");
-		sb.append("\n").append("           ####       ###       ####");
-		sb.append("\n").append("           ####      #####      ####");
-		sb.append("\n").append("            ##########   ##########");
-		sb.append("\n").append("             #########   #########");
-		sb.append("\n").append("               #################");
-		sb.append("\n").append("                ###############");
-		sb.append("\n").append("     ###        #### # # # ####       ###");
-		sb.append("\n").append("    #####        #############       #####");
-		sb.append("\n").append("     #######      ###########     ########");
-		sb.append("\n").append("   ##############    #####    ##############");
-		sb.append("\n").append("  ####################   ##################");
-		sb.append("\n").append("  ###        ############### ######");
-		sb.append("\n").append("      ####     ###############");
-		sb.append("\n").append("    ################## ###################");
-		sb.append("\n").append("   ##############           ##############");
-		sb.append("\n").append("    #######                      ########");
-		sb.append("\n").append("    ####                         #####");
-		_SKULL = sb.toString();
+		_SMILEY = FileUtil.loadContent("/STARTED.ascii");
+		_SKULL = FileUtil.loadContent("/TERIMATED.ascii");
 	}
 	
 
