a7d473baae56061b9ad161b2fb9d0637c4e17673
onghuilam <onghuilam@gmail.com>
2020-04-01 16:46:55 +0800

    increase version

--- src/hl/common/shell/HLProcess.java
+++ src/hl/common/shell/HLProcess.java
@@ -23,7 +23,7 @@ import hl.common.shell.utils.TimeUtil;
 
 public class HLProcess extends HLProcessCmd implements Runnable
 {
-	private final static String _VERSION = "HLProcess alpha v0.78";
+	private final static String _VERSION = "HLProcess alpha v0.79";
 	
 	public static enum ProcessState 
 	{ 
