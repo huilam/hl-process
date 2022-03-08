ca81087fbc96e2cdd2e4f1fe90c53fdf6b9e2bcc
huilam_ong@nec.com.sg <huilam_ong@nec.com.sg>
2022-03-08 12:05:11 +0800

    Adding '-f' for text file support for 'IsHostNameMapped'

--- src/hl/common/shell/plugins/cmd/IsHostNameMapped.java
+++ src/hl/common/shell/plugins/cmd/IsHostNameMapped.java
@@ -1,7 +1,12 @@
 package hl.common.shell.plugins.cmd;
 
+import java.io.File;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import hl.common.shell.utils.FileUtil;
 
 public class IsHostNameMapped {
 	
@@ -10,43 +15,99 @@ public class IsHostNameMapped {
 	{
 		boolean isSyntaxErr = true;
 		
-		if(args.length==1)
+		if(args.length>=1)
 		{
 			isSyntaxErr = false;
 			
 			String sIP = null;
-			String sHostname = args[0];
 			
-			try {
-				try {
-					InetAddress ipAddr =  InetAddress.getByName(sHostname);
-					
-					if(ipAddr!=null)
+			boolean isFileMode = false;
+			List<String> listParamName = new ArrayList<String>();
+			
+			for(String sArg : args)
+			{
+				if(sArg.startsWith("-"))
+				{
+					if(!isFileMode)
 					{
-						sIP = ipAddr.getHostAddress();
+						isFileMode = "-f".equalsIgnoreCase(sArg);
 					}
 				}
-				catch(Throwable t)
+				else 
 				{
-					//t.printStackTrace();
-					sIP = null;
+					listParamName.add(sArg);
 				}
-				
 			}
-			catch(Throwable t2)
-			{
-				//t2.printStackTrace();
-				sIP = null;
-			}	
 			
 			
-			if(sIP!=null && sIP.length()>=4)
+			List<String> listHostName = new ArrayList<String>();
+			
+			if(isFileMode)
 			{
-				System.out.println("[IsHostNameMapped-OK] "+sHostname+" - "+sIP);
+				for(String sParamName : listParamName)
+				{
+				
+					File file = new File(sParamName);
+					
+					if(file.isFile())
+					{
+						String sContent = FileUtil.loadContent(file.getAbsolutePath());
+						
+						StringTokenizer tk1 = new StringTokenizer(sContent, "\n");
+						while(tk1.hasMoreTokens())
+						{
+							String sStr1 = tk1.nextToken().trim();
+							StringTokenizer tk2 = new StringTokenizer(sStr1, ",");
+							while(tk2.hasMoreTokens())
+							{
+								String sStr2 = tk2.nextToken().trim();
+								listHostName.add(sStr2);
+								System.out.println(" Adding "+sStr2);
+							}
+							
+						}
+					}
+				}
 			}
 			else
 			{
-				System.err.println("[IsHostNameMapped-ERR] "+sHostname+" NOT mapped to any IP addresses.");
+				listHostName = listParamName;
+			}
+			
+			System.out.println("[IsHostNameMapped] Checking hostname mapping ...");
+			List<String> listMapErr = new ArrayList<String>();
+			for(String sHostname : listHostName)
+			{
+				if(sHostname!=null && sHostname.trim().length()>0)
+				{
+					try {
+						InetAddress ipAddr =  InetAddress.getByName(sHostname);
+						
+						if(ipAddr!=null)
+						{
+							sIP = ipAddr.getHostAddress();
+							
+							System.out.println("  - [OK] '"+sHostname+"' mapped to "+sIP);
+						}
+					}
+					catch(Throwable t)
+					{
+						listMapErr.add(sHostname);
+						sIP = null;
+						System.err.println("  - [ERR] '"+sHostname+"' NOT mapped to any IP addresses.");
+					}
+				}
+			}
+			
+			
+			String[] sErrMapHost = listMapErr.toArray(new String[listMapErr.size()]);
+			if(sErrMapHost.length==0)
+			{
+				System.out.println("[IsHostNameMapped-OK] All Required hostnames are mapped.");
+			}
+			else
+			{
+				System.err.println("[IsHostNameMapped-ERR] NOT all required hostnames are mapped.");
 			}
 			
 			
@@ -54,8 +115,9 @@ public class IsHostNameMapped {
 		
 		if(isSyntaxErr)
 		{
-			System.out.println("Syntax  : IsHostNameMapped <hostname>");
+			System.out.println("Syntax  : IsHostNameMapped [-f] <hostname>");
 			System.out.println("Example : IsHostNameMapped myhostname");
+			System.out.println("        : IsHostNameMapped -f C:/hostsname.txt");
 		}
 	}
 	
